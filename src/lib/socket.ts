5e39297527b4fc445abc627a01f29c3c2f7dfa31
theanxious9t <tareqkhan441@gmail.com>
2026-04-05 10:37:07 +0530

    Switch Google auth flow from popup to redirect

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,6 +1,8 @@
 import { io } from "socket.io-client";
 
-const socket = io(window.location.origin, {
+const socketUrl = import.meta.env.VITE_SOCKET_URL || window.location.origin;
+
+const socket = io(socketUrl, {
   autoConnect: false,
 });
 
